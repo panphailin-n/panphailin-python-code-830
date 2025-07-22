81b912b9fd6880c07102d273d7f0378af590b527
panphailin nukhunthod <panphailin.n@ku.th>
2025-07-22 11:44:57 +0700

    Age and ATM simulation

--- assignments/week03/lab01.py
+++ assignments/week03/lab01.py
@@ -1,3 +1,4 @@
+"""
 # Complete this program to classify people by age
 age = int(input("Enter age: "))
 
@@ -8,7 +9,15 @@ age = int(input("Enter age: "))
 # 60+: Senior
 
 # Your code here:
-
+"""
 age = int (input("Enter age :"))
-   if age>0<12:
-   print("your Child ")
+if age >=0 and age <=12 :
+    print("Child")
+elif age >=13 and age <=19 :
+    print(" Teenage")
+elif age >=20 and  age <=59:
+    print("Adult")
+elif age >=60 :
+    print("Senior")
+else :
+    print("")
---
+++ assignments/week03/lab02.py
@@ -0,0 +1,60 @@
+"""
+# Complete this ATM simulation
+balance = 1000
+pin = "1234"
+
+entered_pin = input("Enter PIN: ")
+if entered_pin == pin:
+    print("PIN accepted")
+    while True:
+        print("\n1. Check Balance")
+        print("2. Withdraw")
+        print("3. Deposit") 
+        print("4. Exit")
+        
+        choice = input("Choose option: ")
+        
+        # Complete the menu logic here
+        # Your code here:
+        
+else:
+    print("Invalid PIN")
+"""
+balance = 1000
+pin = 1234
+enterad_pin = int(input("Enter PIN :"))
+if enterad_pin == pin:
+    print("PIN accepted")
+    while True :
+        print("\n 1.Cheak Balance")
+        print("2.Withdraw")
+        print("3.Deposit")
+        print("4.Exit")
+        
+        choice = int(input("Choose option"))
+        if choice == "1":
+            print("You balance =",balance)
+        if choice =="2":
+            amount=float(input("How much do you want Withdeaw"))
+            if amount < 0:
+                print("Cannot windraw amount: ")
+            else:
+            balance = balance-amount
+            print("Please cllect your money, and your balance now =",balance)
+        if choice=="3":
+            amount=float(input("Deposit amount:"))
+            if amount <0:
+                print("Cannot deposit less than 0")
+            else:
+            balance=balance-amount
+            print("Your balance now =",balance)
+        if choice =="4":
+            break
+        else :
+            print("Errror")
+            continue
+    
+
+    else:
+        print("Invaid PIN")       
+
