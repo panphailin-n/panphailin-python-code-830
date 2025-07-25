618326234cd089b2f4ba51b49a7074563dd4f377
panphailin nukhunthod <panphailin@ku.th>
2025-07-25 19:27:33 +0700

    hw1-expense-calculator.py

--- assignments/homeworks/hw1/hw1-expense-calculator.py
+++ assignments/homeworks/hw1/hw1-expense-calculator.py
@@ -17,8 +17,8 @@ Total_Fixed=rent_cost+transportation_cost
 Total_Variable=food_budget+entertainment_budget
 Total_Expenses=Total_Fixed+Total_Variable
 Remaining_income=Monthly_income-Total_Expenses
-Emergency_Fund_Amount=Monthly_income*(emergency_fund_percent/100)
-investment_Amount = Monthly_income * (investment_percent / 100)
+Emergency_Fund_Amount=Remaining_income*(emergency_fund_percent/100)
+investment_Amount = Remaining_income * (investment_percent / 100)
 Available_savings=Remaining_income-Emergency_Fund_Amount-investment_Amount
 Expense_ratio=(Total_Expenses/Monthly_income)*100
 
@@ -32,8 +32,8 @@ print(f"Remaining Income :{Remaining_income:.2f}THB")
 print("")
 
 print("===SAVINGS BREAKDOWN===")
-print(f"Emergency Fund ({emergency_fund_percent:.2f}%): {Emergency_Fund_Amount:.2f}THB")
-print(f"Investment ({investment_percent:.2f}%):{investment_Amount:.2f}THB")
+print(f"Emergency Fund ({emergency_fund_percent}%): {Emergency_Fund_Amount:.2f}THB")
+print(f"Investment ({investment_percent}%):{investment_Amount:.2f}THB")
 print(f"Available for Savings:{Available_savings:.2f}THB")
 print("")
 
