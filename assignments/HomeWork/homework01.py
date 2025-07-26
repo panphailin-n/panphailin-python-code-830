396b7c98da075bdf5d1c4e8ac6fe77cf2924c738
panphailin nukhunthod <panphailin@ku.th>
2025-07-26 12:27:53 +0700

    hw1-expense-calculator.py

--- assignments/HomeWork/homework01.py
+++ assignments/HomeWork/homework01.py
@@ -5,44 +5,41 @@ Date :[21/7/2568]
 Purpose:Calculator monthly budget amd savings
 """
 
-Monthly_income = float(input("User Monthly_income in THB :"))
+Monthly_income = float(input("User's Monthly_income in THB :"))
 rent_cost= float(input("Mounthly rent : "))
 food_budget = int(input("Mounthly food budget in THB :"))
 transportation_cost = float(input("Mounthly transportation expenses :"))
 entertainment_budget = int(input("Mounthly entretainment budget :"))
-emergency_fund_percent = float(input("Percentage to save for emergency(e.g.,10.5) :"))
-investment_percent = float(input("Percentage to invest(e.g.,15.0) :"))
+emergency_fund_percent = float(input("Percentage to save for emergency :"))
+investment_percent = float(input("Percentage to invest :"))
 
 Total_Fixed=rent_cost+transportation_cost
 Total_Variable=food_budget+entertainment_budget
 Total_Expenses=Total_Fixed+Total_Variable
 Remaining_income=Monthly_income-Total_Expenses
 Emergency_Fund_Amount=Monthly_income*(emergency_fund_percent/100)
-investment_amount = Monthly_income * (investment_percent / 100)
-Available_savings=emaining_income-Emergency_Fund_Amoun-investment_amount
+investment_Amount = Remaining_income * (investment_percent / 100)
+Available_savings=Remaining_income-Emergency_Fund_Amount-investment_Amount
 Expense_ratio=(Total_Expenses/Monthly_income)*100
 
 print("")
-print("===MOUNTHY BUDGET REPORT===")
+print("===MOUNTHLY BUDGET REPORT===")
 print(f"Income :{Monthly_income:.2f}THB")
 print(f"Fixed Expenses : {Total_Fixed:.2f}THB")
 print(f"Variable Expenses :{Total_Variable:.2f}THB")
-print(f"Total Expenses : {otal_Expenses:.2f}THB")
+print(f"Total Expenses : {Total_Expenses:.2f}THB")
 print(f"Remaining Income :{Remaining_income:.2f}THB")
 print("")
 
 print("===SAVINGS BREAKDOWN===")
-print(f"Emergency Fund ({emergency_fund_percent}%): {emergency_fund_percent:.2f}THB")
-print(f"Investment ({investment_percent}%):{investment_percent:.2f}THB")
-print("Available for Savings:{Available_savings:.2f}THB")
+print(f"Emergency Fund ({emergency_fund_percent:.2f}%): {Emergency_Fund_Amount:.2f}THB")
+print(f"Investment ({investment_percent:.2f}%):{investment_Amount:.2f}THB")
+print(f"Available for Savings:{Available_savings:.2f}THB")
 print("")
 
 print("===ANALYSIS===")
 print(f"Expense Ratio:{Expense_ratio:.2f}%")
 print("")
-
-
-
 
 
 
