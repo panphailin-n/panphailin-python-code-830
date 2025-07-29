55659413bb98747f336ca36a7b150998bf494fae
panphailin nukhunthod <panphailin.n@ku.th>
2025-07-29 12:15:32 +0700

    Number List Operations

--- assignments/week04/lab02.py
+++ assignments/week04/lab02.py
@@ -14,33 +14,46 @@ List of numbers greater than the average
 Show statistics: sum, average, min, max
 
 """
-"""
+
+
 
 def number_operations():
     numbers = []
-    
-    # Get 10 numbers from user
+     # Get 10 numbers from user
     print("Enter 10 numbers:")
     for i in range(10):
         # Your code here
-        pass
-    
+        number=int(input("Insert number[" + i +"] :"))
+        numbers.append(number)
     # Display original list
     print(f"Original numbers: {numbers}")
-    
-    # Create filtered lists
-    even_numbers = # Your code here
-    odd_numbers = # Your code here
-    
+     # Create filtered lists
+    even_numbers = []
+    odd_numbers = [] # Your code here
+
     # Calculate average
-    average = # Your code here
+    average =sum(numbers) /10.0 # Your code here
     
     # Numbers greater than average
-    above_average = # Your code here
+    above_average = []# Your code here
     
+    for i in range(10):
+        if numbers[i]%2 == 0:
+            even_numbers.append(number[i])
+        else:
+            odd_numbers.append(number[i])
+        if numbers[i]> average :
+            above_average.append(numbers[i])
     # Display results
     # Your code here
-
+    print("Enen Number list :",even_numbers)
+    print("Odd Numbers list :",odd_numbers)
+    print("Above Averagr list :",above_average)
+    print("Sum:",sum(numbers))
+    print("Average :",average)
+    print("Min:",min(numbers))
+    print("Max:",max(numbers))
+   
 if __name__ == "__main__":
     number_operations()
-    """
+    
