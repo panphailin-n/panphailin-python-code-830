c09151fecf44c90601dfeff65fc9bee9ba0828e3
panphailin nukhunthod <panphailin.n@ku.th>
2025-07-29 11:41:46 +0700

    ersonal Information Manager  and Number List Operations

--- assignments/week04/lab01.py
+++ assignments/week04/lab01.py
@@ -22,25 +22,25 @@ def personal_info_manager():
         choice = input("What do you want to do ?(1:display,2:addhobby,3:remove hobby,4:edit age,5:exite)")
         # Your code here
         if choice =="1":
-        print("Name :",person[0])
-        print(f"Age:{person[1]}")
-        print("Citty :"+person[2])   
-        print("Country:",person[3])
-        print("Hobbies:",hobbies)
-#append hobby
+                print("Name :",person[0])
+                print(f"Age:{person[1]}")
+                print("Citty :"+person[2])   
+                print("Country:",person[3])
+                print("Hobbies:",hobbies)
+    #append hobby
         elif choice =="2":
-        hobby=input("What is your new hobbies:")
-        hobbies.append[hobby]
-    #remove hobby
+            hobby=input("What is your new hobbies:")
+            hobbies.append(hobby)
+        #remove hobby
         elif choice =="3":
-        hobbies.pop()
+            hobbies.pop()
         elif choice =="4":
-        person_list = list(person)
-        age = input("How old are you? :")
-        person_list[1]=age
-        person = tuple(person_list)
+            person_list = list(person)
+            age = input("How old are you? :")
+            person_list[1]=age
+            person = tuple(person_list)
         elif choice =="5":
-        break
- 
+            break
+    
 if __name__ == "__main__":
     personal_info_manager()
