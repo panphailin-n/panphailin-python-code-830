d3254051b929810430001ddf04c4fd6851ce7de7
panphailin nukhunthod <panphailin.n@ku.th>
2025-09-23 11:48:50 +0700

    .

--- assignments/week09/random_quiz-1.py
+++ assignments/week09/random_quiz-1.py
@@ -25,3 +25,21 @@ Example
     Congratulations! You won in 3 attempts!
 
 """
+import random
+
+def test_random():
+    random_number = random.randint(1,10)
+    for i in range(1,7):
+        guess_number = input(f"Attempt {i}/6 - Enter your guess 1-10:")
+        guess_number =int(guess_number)
+
+        if random_number == guess_number:
+                print("You win!^_^ \n eieiei.")
+        elif random_number < guess_number :
+                print("Too much!*_* Try again.")
+        elif random_number > guess_number :
+                print("Too law!-_- Try again.")
+print("\n")
+print("==Welcome to the number guessing game.==")  
+print("----------------------------------------")        
+test_random()
