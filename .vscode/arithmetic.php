30a654c2b04f4c25616cf86d22d7e959e92e97c7
panphailin nukhunthod <panphailin.m@.ku.th>
2025-12-11 11:22:12 +0700

    .

--- .vscode/arithmetic.php
+++ .vscode/arithmetic.php
@@ -6,19 +6,19 @@ $a=$y;
 echo '$a=10 $b=5 -> a=b -> a=' ;
 echo $a;
 
-echo '$a= $a + 5 = ';
+echo 'a= a + 5 = ';
 echo $a += 5 ;
 
-echo '$a= $a - 5 = ';
+echo 'a= a - 5 = ';
 echo  $a -= 5 ;
 
-echo '$a= $a * 5 = ';
+echo 'a= a * 5 = ';
 echo $a *= 5 ;
 
-echo '$a= $a / 5 = ';
+echo 'a= $a / 5 = ';
 echo $a /= 5 ;
 
-echo '$a= $a % 5 = ';
+echo '$a= a % 5 = ';
 echo $a %= 5 ;
 
 ?>
